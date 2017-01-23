5b8c00c7b7133dcd28967aa347cd3abd4473b799
Khari Ollivierre <ollivierre.khari@gmail.com>
2017-01-23 16:07:40 -0500

    Changed offset radius to 1

--- proj1_mesh/src/mainsrc/main.cpp
+++ proj1_mesh/src/mainsrc/main.cpp
@@ -467,7 +467,7 @@ int midPoint(int p1, int p2, std::multimap<long, int> *midPointIndices, std::vec
           STVector3 mid = STVector3((point1.x - point2.x)/2, (point1.y - point2.y)/2, (point1.z - point2.z)/2);
 
           std::cout<<"    Offset starts here\n";
-          offset(mid, vertices, (1.0 + sqrtf(5.0))/2.0);
+          offset(mid, vertices, 1);
           index = vertices->size()-1;
           midPointIndices->insert(std::pair<long, int>(key, index));
           std::cout<<"    Midpoint added, index = " << index << "\n";
